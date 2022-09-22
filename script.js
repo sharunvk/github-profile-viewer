570b4c8dc8b3c64bc3f472a1890fd1eef33a8ab0
sharun13 <sharunvk@gmail.com>
2022-09-22 18:34:38 +0530

    Fetched data from the API and showing the fetched data in the view

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
-angular.module("MyApp", ['ngMaterial'])
-    .run(function() {
-        console.log("ready");
+angular.module("MyApp", ['ngMaterial', 'ngRoute'])
+    .controller("inputController", function ($scope, $http) {
+        $scope.searchUser = () => {
+            const options = {
+                method: "GET",
+                url: `https://api.github.com/search/users?q=${$scope.user}`,
+                headers: {
+                    Authorization: "Bearer " + "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",
+                },
+            };
+            $scope.usersArray = [];
+            $http(options)
+                .then((response) => {
+                    $scope.usersArray = response.data.items;
+                    $scope.usersArray.forEach((element, index) => {
+                        const singleUserOptions = {
+                            method: "GET",
+                            url: `http://api.github.com/users/${element.login}`,
+                            headers: {
+                                Authorization: "Bearer " + "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",
+                            },
+                        }
+                        $http(singleUserOptions)
+                            .then((detailedData) => {
+                                let detailedUserData = detailedData.data;
+                                $scope.usersArray[index] = {...$scope.usersArray[index], ...detailedUserData};
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                            });
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+
+        $scope.clearInput = () => {
+            $scope.user = "";
+            $scope.usersArray = [];
+        }
     });
